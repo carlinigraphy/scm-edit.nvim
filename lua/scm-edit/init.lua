ba263b53ab4d72237b716f56610b98f24e4ef8a6
marcus <marcus@carlini.com>
2024-04-23 19:57:24 -0700

    fix: buffer-only mappings
    Ran into situations in which I'm editing multiple filetypes, and it's
    retaining scheme bindings in non-scheme files.

--- lua/scm-edit/init.lua
+++ lua/scm-edit/init.lua
@@ -3,23 +3,25 @@ local parens  = require("scm-edit.parens")
 
 return {
    setup = function()
+      local opts = { buffer=true }
+
       -- Preserve default bindings w/ currently unused g_ maps.
-      vim.keymap.set({'n', 'x', 'o'}, 'gb', [[normal! b]])
-      vim.keymap.set({'n', 'x', 'o'}, 'gB', [[normal! B]])
-      vim.keymap.set({'n', 'x', 'o'}, 'gw', [[normal! w]])
-      vim.keymap.set({'n', 'x', 'o'}, 'gW', [[normal! W]])
+      vim.keymap.set({'n', 'x', 'o'}, 'gb', [[normal! b]], opts)
+      vim.keymap.set({'n', 'x', 'o'}, 'gB', [[normal! B]], opts)
+      vim.keymap.set({'n', 'x', 'o'}, 'gw', [[normal! w]], opts)
+      vim.keymap.set({'n', 'x', 'o'}, 'gW', [[normal! W]], opts)
 
-      vim.keymap.set({'n', 'x', 'o'}, 'b',  motions.prev_element_start)
-      vim.keymap.set({'n', 'x', 'o'}, 'w',  motions.next_element_start)
+      vim.keymap.set({'n', 'x', 'o'}, 'b',  motions.prev_element_start, opts)
+      vim.keymap.set({'n', 'x', 'o'}, 'w',  motions.next_element_start, opts)
 
-      vim.keymap.set({'n', 'x'}     , 'e',  motions.next_element_end)
-      vim.keymap.set({'n', 'x'}     , 'ge', motions.prev_element_end)
+      vim.keymap.set({'n', 'x'}, 'e',  motions.next_element_end, opts)
+      vim.keymap.set({'n', 'x'}, 'ge', motions.prev_element_end, opts)
 
-      vim.keymap.set({'n', 'x', 'o'}, 'B',  motions.prev_form_start)
-      vim.keymap.set({'n', 'x', 'o'}, 'W',  motions.next_form_start)
+      vim.keymap.set({'n', 'x', 'o'}, 'B',  motions.prev_form_start, opts)
+      vim.keymap.set({'n', 'x', 'o'}, 'W',  motions.next_form_start, opts)
 
-      vim.keymap.set('i', '<C-j>', parens.close_all)
-      vim.keymap.set('i', '<C-k>', parens.close_one)
-      vim.keymap.set('n', 'st'   , parens.toggle)
+      vim.keymap.set('i', '<C-j>', parens.close_all, opts)
+      vim.keymap.set('i', '<C-k>', parens.close_one, opts)
+      vim.keymap.set('n', 'st'   , parens.toggle   , opts)
    end,
 }
